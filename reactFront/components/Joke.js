7185616f736ed46c3449e189bcef800f43273e69
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-05-13 23:56:43 +0100

    add fetch to Joke

--- reactFront/components/Joke.js
+++ reactFront/components/Joke.js
@@ -4,31 +4,47 @@ class Joke extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        liked: ""
+        liked: "",
+        jokes: []
       }
       this.like = this.like.bind(this);
+      this.serverRequest = this.serverRequest.bind(this);
     }
   
     like() {
-      // ... we'll add this block later
+      let joke = this.props.joke;
+      this.serverRequest(joke);
+    }
+
+    serverRequest(joke) {
+      $.post(
+        "http://localhost:3000/api/jokes/like/" + joke.id,
+        { like: 1 },
+        res => {
+          console.log("res... ", res);
+          this.setState({ liked: "Liked!", jokes: res });
+          this.props.jokes = res;
+        }
+      );
     }
   
     render() {
       return (
         <div className="col-xs-4">
           <div className="panel panel-default">
-            <div className="panel-heading">#{this.props.joke.id} <span className="pull-right">{this.state.liked}</span></div>
-            <div className="panel-body">
-              {this.props.joke.joke}
+            <div className="panel-heading">
+              #{this.props.joke.id}{" "}
+              <span className="pull-right">{this.state.liked}</span>
             </div>
+            <div className="panel-body">{this.props.joke.joke}</div>
             <div className="panel-footer">
               {this.props.joke.likes} Likes &nbsp;
               <a onClick={this.like} className="btn btn-default">
-                <span className="glyphicon glyphicon-thumbs-up"></span>
+                <span className="glyphicon glyphicon-thumbs-up" />
               </a>
             </div>
           </div>
-        </div>
+      </div>
       )
     }
   }
